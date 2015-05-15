327c80e9ed766e62d26090e110ffefc9f77b3c07
olav <olav@bjorkoy.com>
2015-05-15 10:01:50 +0200

    Example app name

--- example/src/androidTest/java/com/example/activity/ExampleEspressoTest.java
+++ example/src/androidTest/java/com/example/activity/ExampleEspressoTest.java
@@ -1,8 +1,10 @@
 package com.example.activity;
 
-import com.example.R;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+
+import com.example.R;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -22,6 +24,6 @@ public class ExampleEspressoTest extends ActivityInstrumentationTestCase2<Exampl
     }
 
     public void testActivityShouldHaveText() throws InterruptedException {
-        onView(withId(R.id.text)).check(matches(withText("Hello Espresso!")));
+        onView(withId(R.id.text)).check(matches(withText("Hello Appstax!")));
     }
 }
