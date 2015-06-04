e9e27b082935428437c8ec9438dfe86c4ad67cd6
olav <olav@bjorkoy.com>
2015-06-04 16:42:17 +0200

    Improve example app

--- examples/basic/app/src/main/java/com/appstax/basic/MainActivity.java
+++ examples/basic/app/src/main/java/com/appstax/basic/MainActivity.java
@@ -1,18 +1,23 @@
 package com.appstax.basic;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.appstax.*;
+import com.appstax.AxException;
+import com.appstax.AxUser;
 import com.appstax.android.Appstax;
 import com.appstax.android.Callback;
 
-import java.util.UUID;
-
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String APPSTAX_KEY = "YourAppKey";
+    private static final String APPSTAX_URL = "https://appstax.com/api/latest/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +26,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Set your app key.
-        Appstax.setAppKey("YourAppKey");
+        Appstax.setAppKey(APPSTAX_KEY);
+        Appstax.setApiUrl(APPSTAX_URL);
 
-        // Get username and password from input.
-        String username = UUID.randomUUID().toString();
-        String password = UUID.randomUUID().toString();
-
-        // Create a new user and log in.
-        Appstax.signup(username, password, new Callback<AxUser>() {
-            public void onSuccess(AxUser user) {
-                showMessage("Hello, " + Appstax.getCurrentUser().getUsername());
-            }
-
-            public void onError(AxException e) {
-                showMessage(e.getMessage());
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signup();
             }
         });
     }
 
-    private void showMessage(String message) {
-        final TextView text = (TextView) findViewById(R.id.text);
-        text.setText(message);
+    protected void signup() {
+        Appstax.signup(email(), password(), new Callback<AxUser>() {
+            public void onSuccess(AxUser output) {
+                message("welcome", Appstax.getCurrentUser().getUsername());
+            }
+            public void onError(AxException e) {
+                message("error", e.getMessage());
+            }
+        });
+    }
+
+    protected void message(String title, String message) {
+        new AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setCancelable(true)
+            .create().show();
+    }
+
+    protected String email() {
+        EditText view = (EditText) findViewById(R.id.email);
+        return view.getText().toString();
+    }
+
+    protected String password() {
+        EditText view = (EditText) findViewById(R.id.password);
+        return view.getText().toString();
     }
 
 }
