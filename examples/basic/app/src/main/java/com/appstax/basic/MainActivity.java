4619d5b889dc815e6eb2da480116b0ba56d1a781
olav <olav@bjorkoy.com>
2015-06-04 11:23:52 +0200

    Material Theme

--- examples/basic/app/src/main/java/com/appstax/basic/MainActivity.java
+++ examples/basic/app/src/main/java/com/appstax/basic/MainActivity.java
@@ -1,25 +1,25 @@
 package com.appstax.basic;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
-import com.appstax.AxException;
-import com.appstax.AxUser;
+import com.appstax.*;
 import com.appstax.android.Appstax;
 import com.appstax.android.Callback;
 
 import java.util.UUID;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Set your app key.
         Appstax.setAppKey("YourAppKey");
@@ -30,43 +30,19 @@ public class MainActivity extends ActionBarActivity {
 
         // Create a new user and log in.
         Appstax.signup(username, password, new Callback<AxUser>() {
-
             public void onSuccess(AxUser user) {
-                showMessage("Welcome", "Hello, " + user.getUsername());
+                showMessage("Hello, " + Appstax.getCurrentUser().getUsername());
             }
 
             public void onError(AxException e) {
-                showMessage("Error", e.getMessage());
+                showMessage(e.getMessage());
             }
-
         });
     }
 
-    private void showMessage(String title, String message) {
+    private void showMessage(String message) {
         final TextView text = (TextView) findViewById(R.id.text);
-        this.setTitle(title);
         text.setText(message);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
