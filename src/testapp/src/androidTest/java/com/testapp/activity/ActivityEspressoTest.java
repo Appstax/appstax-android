bac1a5387dc9a23f5b6ab2881a3fec48b1c516d3
olav <olav@bjorkoy.com>
2015-06-04 15:52:12 +0200

    Simplify testapp

--- src/testapp/src/androidTest/java/com/testapp/activity/ActivityEspressoTest.java
+++ src/testapp/src/androidTest/java/com/testapp/activity/ActivityEspressoTest.java
@@ -4,16 +4,12 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
 import com.appstax.AxException;
+import com.appstax.AxFile;
 import com.appstax.AxObject;
 import com.appstax.android.Appstax;
 import com.appstax.android.Callback;
 
 import java.util.List;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 public class ActivityEspressoTest extends ActivityInstrumentationTestCase2<Activity> {
@@ -21,9 +17,7 @@ public class ActivityEspressoTest extends ActivityInstrumentationTestCase2<Activ
     private static final String APP_KEY_1 = "YourApiKey";
     private static final String APP_KEY_2 = "SomeAppKey";
     private static final String API_URL_1 = com.appstax.Ax.getApiUrl();
-
-    private static final String COLLECTION_COUNT = "CountCollection";
-    private static final String COLLECTION_BLANK = "BlankCollection";
+    private static final String COLLECTION = "BlankCollection";
 
     public ActivityEspressoTest() {
         super(Activity.class);
@@ -37,10 +31,6 @@ public class ActivityEspressoTest extends ActivityInstrumentationTestCase2<Activ
         Appstax.setApiUrl(API_URL_1);
     }
 
-    public void testActivityShouldHaveText() throws InterruptedException {
-        onView(withText(COLLECTION_BLANK)).check(matches(isDisplayed()));
-    }
-
     public void testAppKey() {
         assertEquals(APP_KEY_1, Appstax.getAppKey());
         Appstax.setAppKey(APP_KEY_2);
@@ -52,19 +42,29 @@ public class ActivityEspressoTest extends ActivityInstrumentationTestCase2<Activ
     }
 
     public void testObjectCreate() {
-        AxObject object = new AxObject(COLLECTION_COUNT);
+        AxObject object = new AxObject(COLLECTION);
         object.put("title", "hello");
         object.put("count", 42);
-        assertEquals(COLLECTION_COUNT, object.getCollection());
+        assertEquals(COLLECTION, object.getCollection());
         assertEquals("hello", object.get("title"));
         assertEquals(42, object.get("count"));
     }
 
+    public void testFileCreate() {
+        AxObject object = new AxObject(COLLECTION);
+        AxFile file = new AxFile("123", "123".getBytes());
+        object.put("file", file);
+        assertNull(object.getId());
+        assertNotNull(object.getFile("file").getName());
+        assertNull(object.getFile("file").getUrl());
+    }
+
     public void testObjectSave() {
-        Appstax.save(new AxObject(COLLECTION_BLANK), new Callback<AxObject>() {
+        Appstax.save(new AxObject(COLLECTION), new Callback<AxObject>() {
             public void onSuccess(AxObject output) {
                 assertTrue(false);
             }
+
             public void onError(AxException e) {
                 assertTrue(e.getMessage().startsWith("Not authorized."));
             }
@@ -72,10 +72,11 @@ public class ActivityEspressoTest extends ActivityInstrumentationTestCase2<Activ
     }
 
     public void testObjectRemove() {
-        Appstax.remove(new AxObject(COLLECTION_BLANK), new Callback<AxObject>() {
+        Appstax.remove(new AxObject(COLLECTION), new Callback<AxObject>() {
             public void onSuccess(AxObject output) {
                 assertTrue(false);
             }
+
             public void onError(AxException e) {
                 assertTrue(e.getMessage().startsWith("Not authorized."));
             }
@@ -83,7 +84,7 @@ public class ActivityEspressoTest extends ActivityInstrumentationTestCase2<Activ
     }
 
     public void testObjectFilter() {
-        Appstax.filter(COLLECTION_BLANK, "Age > 42", new Callback<List<AxObject>>() {
+        Appstax.filter(COLLECTION, "Age > 42", new Callback<List<AxObject>>() {
             public void onSuccess(List<AxObject> output) {
                 assertTrue(false);
             }
--- src/testapp/src/main/java/com/testapp/activity/Activity.java
+++ src/testapp/src/main/java/com/testapp/activity/Activity.java
@@ -1,83 +1,21 @@
 package com.testapp.activity;
 
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
-import com.appstax.AxException;
-import com.appstax.AxObject;
-import com.appstax.AxUser;
 import com.appstax.android.Appstax;
-import com.appstax.android.Callback;
 import com.testapp.R;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+public class Activity extends android.app.Activity {
 
-public class Activity extends ListActivity {
-
-    private static final String APP_KEY = "YourAppKey";
+    private static final String APP_KEY = "key";
     private static final String API_URL = "https://appstax.com/api/latest/";
-    private static final String COLLECTION_NAME = "BlankCollection";
-
-    ArrayList<String> items = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
-    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
-
-        // Setup activity.
-        this.text = (TextView) findViewById(R.id.title);
-        this.text.setText(COLLECTION_NAME);
-        this.adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-        this.setListAdapter(adapter);
-
-        // Setup Appstax.
         Appstax.setAppKey(APP_KEY);
         Appstax.setApiUrl(API_URL);
-
-        String username = UUID.randomUUID().toString();
-        String password = UUID.randomUUID().toString();
-
-        // Create user and make objects.
-        Appstax.signup(username, password, new Callback<AxUser>() {
-            public void onSuccess(AxUser output) {
-                createObject();
-                listObjects();
-            }
-            public void onError(AxException e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    private void createObject() {
-        AxObject object = new AxObject(COLLECTION_NAME);
-        object.grantPublic("read", "update");
-        Appstax.save(object, null);
-        Appstax.refresh(object, null);
-    }
-
-    private void listObjects() {
-        Appstax.find(COLLECTION_NAME, new Callback<List<AxObject>>() {
-            public void onSuccess(List<AxObject> objects) {
-                addToList(objects);
-            }
-            public void onError(AxException e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    private void addToList(List<AxObject> objects) {
-        for (AxObject object : objects) {
-            adapter.add(object.getId());
-        }
     }
 
 }
